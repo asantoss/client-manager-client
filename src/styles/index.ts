cfb68099feeb5d0fb551e3b74e89153a3fd8b2d2
alexander <alexsantosantana@live.com>
2020-02-29 09:17:49 -0500

    new fonts

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -13,9 +13,13 @@ export const GlobalStyle = createGlobalStyle`
 body {
   background-color: ${({ theme }) => theme.colors.background};
   font-size: 12px;
-  font-family: 'Hind', sans-serif;
   max-width: 600px;
   margin: 70px auto; 
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 }
 
 
